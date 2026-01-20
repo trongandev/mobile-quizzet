0f5ebda35e1b8f7442d75c87e9e3ba2004c12bc4
Trong An <troandev@gmail.com>
2026-01-20 15:43:28 +0700

    new

---
+++ app.config.ts
@@ -0,0 +1,88 @@
+import type { ConfigContext, ExpoConfig } from '@expo/config';
+import type { AppIconBadgeConfig } from 'app-icon-badge/types';
+
+import { ClientEnv, Env } from './env';
+
+const appIconBadgeConfig: AppIconBadgeConfig = {
+  enabled: Env.APP_ENV !== 'production',
+  badges: [
+    {
+      text: Env.APP_ENV,
+      type: 'banner',
+      color: 'white',
+    },
+    {
+      text: Env.VERSION.toString(),
+      type: 'ribbon',
+      color: 'white',
+    },
+  ],
+};
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'Quizzet',
+  description: `Quizzet Mobile App`,
+  owner: 'angutboiz',
+  scheme: Env.SCHEME,
+  slug: 'quizzet',
+  version: Env.VERSION.toString(),
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  updates: {
+    url: 'https://u.expo.dev/4f37bab8-e8f6-4f4e-af32-392f9ec35e1d',
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: Env.BUNDLE_ID,
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+    },
+  },
+  experiments: {
+    typedRoutes: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#2E3C4B',
+    },
+    package: Env.PACKAGE,
+  },
+  web: {
+    favicon: './assets/favicon.png',
+    bundler: 'metro',
+  },
+  plugins: [
+    [
+      'expo-splash-screen',
+      {
+        backgroundColor: '#2E3C4B',
+        image: './assets/splash-icon.png',
+        imageWidth: 150,
+      },
+    ],
+    [
+      'expo-font',
+      {
+        fonts: ['./assets/fonts/Inter.ttf'],
+      },
+    ],
+    'expo-localization',
+    'expo-router',
+    ['app-icon-badge', appIconBadgeConfig],
+    ['react-native-edge-to-edge'],
+  ],
+  extra: {
+    ...ClientEnv,
+    eas: {
+      projectId: '4f37bab8-e8f6-4f4e-af32-392f9ec35e1d',
+    },
+  },
+});
